ad8c83b00c3db05b3d3710d99f476a5c23ee524a
Szbzltn99 <szabozltn99@gmail.com>
2021-06-30 14:16:01 +0200

    feat: added function for csv reading

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.csv_read import csv_read
 
 if __name__ == '__main__':
-    csv_read(
+    print(csv_read(
         'https://github.com/EKU-Summer-2021/intelligent_system_data/blob/main/Intelligent%20System%20Data/TSP/10.csv'
-        '?raw=true')
+        + '?raw=true'))
