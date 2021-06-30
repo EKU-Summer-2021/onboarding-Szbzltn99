8179851e666705aae50e7c195135dbf9351782bb
Szbzltn99 <szabozltn99@gmail.com>
2021-06-30 11:16:35 +0200

    feat: added function for csv reading

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import numpy as np
 from src import Polynomial
+from src.csv_read import csv_read
+
 
 if __name__ == '__main__':
     coeffs = np.array([1,0,0])
     polynom = Polynomial(coeffs)
     print(polynom.evaluate(3))
     print(polynom.roots())
+    csv_read('https://github.com/EKU-Summer-2021/intelligent_system_data/blob/main/Intelligent%20System%20Data/TSP/10.csv?raw=true')
