8a2c9514e86fd8334fedd5d75e5daa8dfd276c8a
Szbzltn99 <szabozltn99@gmail.com>
2021-06-30 11:39:38 +0200

    feat: added function for csv reading

--- src/csv_read.py
+++ src/csv_read.py
@@ -2,8 +2,3 @@ def csv_read(url):
     import pandas as pd
     df = pd.read_csv(url, header=None)
     print(df)
-
-
-csv_read('https://github.com/EKU-Summer-2021/intelligent_system_data/blob/main/Intelligent%20System%20Data/TSP/10.csv?raw=true')
-
-
