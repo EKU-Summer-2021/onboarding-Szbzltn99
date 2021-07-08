ca444e4b3841d95070ac52b1297e7e778bc0e8e1
Szbzltn99 <szabozltn99@gmail.com>
2021-07-08 15:21:04 +0200

    feat: Created solve function for tsp problem.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from src.csv_read import csv_read
-
+from src.tsp_class import TSP
+from src.sa_solver import SAS
 if __name__ == '__main__':
-    print(csv_read(
-        'https://github.com/EKU-Summer-2021/intelligent_system_data/blob/main/Intelligent%20System%20Data/TSP/10.csv'
-        + '?raw=true'))
+    url = 'https://github.com/EKU-Summer-2021/intelligent_system_data/' \
+          'blob/main/Intelligent%20System%20Data/TSP/10.csv?raw=true'
+    tsp = TSP(url)
+    sas = SAS(tsp)
+    sas.solve()
--- tests/csv_read_test.py
+++ tests/csv_read_test.py
@@ -7,6 +7,8 @@ class CsvReadTest(unittest.TestCase):
     def test_csv_read_test(self):
 
         expected = True
-        actual = isinstance(csv_read('https://github.com/EKU-Summer-2021/intelligent_system_data/blob/main/Intelligent%20System%20Data/TSP/10.csv'
-        '?raw=true'), pandas.core.frame.DataFrame)
+        actual = isinstance(csv_read(
+            'https://github.com/EKU-Summer-2021'
+            '/intelligent_system_data/blob/main/Intelligent%20System%20Data/TSP/10.csv'
+            '?raw=true'), pandas.DataFrame)
         self.assertEqual(expected, actual)
